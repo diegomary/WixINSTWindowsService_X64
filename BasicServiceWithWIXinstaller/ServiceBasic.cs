782d2b588dc2b6d84b04727d9590d91014b72734
diego <diego@dmm888.com>
2014-03-02 19:52:22 +0000

    First Commit

---
+++ BasicServiceWithWIXinstaller/ServiceBasic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicServiceWithWIXinstaller
+{
+    public partial class ServiceBasic : ServiceBase
+    {
+        public ServiceBasic()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+
+
+
+
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
